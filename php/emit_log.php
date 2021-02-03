a2774f6da47a793a9b40fa15c4f8733a988580e9
侯鹏 <houpeng@doushen.com>
2021-02-03 18:28:01 +0800

    验证队列的问题

--- php/emit_log.php
+++ php/emit_log.php
@@ -21,4 +21,26 @@ echo ' [x] Sent ', $data, "\n";
 
 $channel->close();
 $connection->close();
+
+
+/**
+ * 现象：
+ * 1. 声明了exchange,消息直接发送到了exchange中
+ * 2. 没有声明队列，也没有绑定队列
+ */
+
+/**
+ * 实践：
+ * 1. 执行`php emit_log.php`,通过管理后台查看logs交换机是否有和队列的绑定关系
+ * 2. 查看是否有队列接收到了发送的消息
+ */
+
+/**
+ * 结论：
+ * 1. 生产者总是将消息发给exchange,甚至不用关心exchange有没有绑定的队列，消息到底发到了哪个队列
+ * 官网文档地址：[https://www.rabbitmq.com/tutorials/tutorial-three-python.html](https://www.rabbitmq.com/tutorials/tutorial-three-python.html)
+ * The core idea in the messaging model in RabbitMQ is that the producer never sends any messages directly to a queue. Actually, quite often the producer doesn't even know if a message will be delivered to any queue at all.
+ * 2. 消息在没有到达队列内时，消息丢失了，说明exchange是不具有存储能力的
+ */
 ?>
+
--- php/receive_logs.php
+++ php/receive_logs.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once __DIR__ . '/vendor/autoload.php';
+
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 $connection = new AMQPStreamConnection('59.110.213.203', 5672, 'guest', 'guest');
@@ -9,6 +10,7 @@ $channel = $connection->channel();
 $channel->exchange_declare('logs', 'fanout', false, false, false);
 
 list($queue_name, ,) = $channel->queue_declare("", false, false, true, false);
+//list($queue_name, ,) = $channel->queue_declare("", false, false, true, true);
 
 $channel->queue_bind($queue_name, 'logs');
 
@@ -26,4 +28,26 @@ while ($channel->is_consuming()) {
 
 $channel->close();
 $connection->close();
+
+/**
+ * 现象
+ * 1. 声明队列时使用的空字符
+ */
+
+/**
+ * 实践步骤
+ * 1. 执行`php receive_logs.php`,通过管理后台查看exchange下是否有队列绑定
+ * 2. 查看队列是否能接收消息
+ * 3. 将队列的参数`auto_delete`改为`true`
+ *
+ */
+
+/**
+ * 结论
+ * 1. 声明队列的时候，使用空字符，可以生成一个名称为`amqp.gen-xxx`形式的临时队列
+ * 2. 对于临时队列的使用场景为：
+ *    a. 消费者启动时创建的，为该消费者独有，其他交换机和消费者无法使用,所以参数`exclusive`设置为`true`
+ *    b. 消费者关闭后，可以将`auto_delete`参数设置为`true`，自动将队列删除
+ */
+
 ?>
